8eb0a31db8f6e1ce785b8cd656bd19ad60d7e067
hasibXtreme <157557464+hasibXtreme@users.noreply.github.com>
2026-08-22 13:04:31 +0600

    Update API endpoint in loginPage.jsx

--- Front_end/src/pages/loginPage.jsx
+++ Front_end/src/pages/loginPage.jsx
@@ -16,7 +16,7 @@ export const Loginpage = ({ onloginsuccess }) => {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch('http://localhost:3000/api/login', {
+      const response = await fetch('/api/login', {
         method: 'POST',
         headers: { 'content-type': 'application/json' },
         credentials: 'include',
