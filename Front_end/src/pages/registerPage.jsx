ff817c638931971168a924ca5ffe4e5f13a4cc38
hasibXtreme <157557464+hasibXtreme@users.noreply.github.com>
2026-08-22 13:05:32 +0600

    Update registerPage.jsx

--- Front_end/src/pages/registerPage.jsx
+++ Front_end/src/pages/registerPage.jsx
@@ -18,7 +18,7 @@ export const Registerpage = () => {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch('http://localhost:3000/api/register', {
+      const response = await fetch('/api/register', {
         method: 'POST',
         headers: { 'content-type': 'application/json' },
         body: JSON.stringify({ name, email, password, role }),
